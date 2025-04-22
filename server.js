613fea3417cf26709a546d02a3d149e6df7a5ced
soumenmt <soumenmt@gmail.com>
2025-04-21 19:16:51 -0600

    Configure Puppeteer for Heroku

--- server.js
+++ server.js
@@ -57,7 +57,11 @@ app.post("/generate-ui-mockup", async (req, res) => {
     const imageId = uuidv4();
     const imagePath = `static/${imageId}.png`;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/app/.apt/usr/bin/google-chrome',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true,
+    });
     const page = await browser.newPage();
     await page.setContent(fullHtml);
     await page.setViewport({ width: 400, height: 600 });
